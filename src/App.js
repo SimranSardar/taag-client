c2874a9a5daef780faac641ec384653715e504ba
Anurag Pal <apal895@gmail.com>
2022-07-09 16:30:41 +0530

    components added, basic styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-function App() {
-  return <div className="App">Hello World!</div>;
-}
+import styles from "./App.scss";
+
+const App = () => {
+  return <div className={styles.container}>Hello World!</div>;
+};
 
 export default App;
