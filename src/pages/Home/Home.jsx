84931cb4a09c4ad1cc61da3eb9f37d0530b9e58d
Anurag Pal <apal895@gmail.com>
2022-07-16 17:04:28 +0530

    added table

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,202 +8,86 @@ import { Button as AntButton, Input, Space, Table } from "antd";
 
 import Highlighter from "react-highlight-words";
 import { MainLayout } from "../../layouts";
+import CustomTable from "../../components/CustomTable/CustomTable";
+
 const data = [
   {
-    key: "1",
+    id: "1",
     name: "John Brown",
-    age: 32,
-    address: "New York No. 1 Lake Park",
-  },
-  {
-    key: "2",
-    name: "Joe Black",
-    age: 42,
-    address: "London No. 1 Lake Park",
-  },
-  {
-    key: "3",
-    name: "Jim Green",
-    age: 32,
-    address: "Sidney No. 1 Lake Park",
-  },
-  {
-    key: "4",
-    name: "Jim Red",
-    age: 32,
-    address: "London No. 2 Lake Park",
+    link: "https://google.com",
+    followers: "20k",
+    avgViews: "100k",
+    deliverable: "YouTube",
+    commercialCreator: "150k",
   },
 ];
 
 const Home = () => {
   const [progress, setProgress] = useState(30);
-  const [searchText, setSearchText] = useState("");
-  const [searchedColumn, setSearchedColumn] = useState("");
-  const searchInput = useRef(null);
-
-  const handleSearch = (selectedKeys, confirm, dataIndex) => {
-    confirm();
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
-  };
-
-  const handleReset = (clearFilters) => {
-    clearFilters();
-    setSearchText("");
-  };
-
-  const getColumnSearchProps = (dataIndex) => ({
-    filterDropdown: ({
-      setSelectedKeys,
-      selectedKeys,
-      confirm,
-      clearFilters,
-    }) => (
-      <div
-        style={{
-          padding: 8,
-        }}
-      >
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={(e) =>
-            setSelectedKeys(e.target.value ? [e.target.value] : [])
-          }
-          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-          style={{
-            marginBottom: 8,
-            display: "block",
-          }}
-        />
-        <Space>
-          <AntButton
-            type="primary"
-            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-            icon={<SearchOutlined />}
-            size="small"
-            style={{
-              width: 90,
-            }}
-          >
-            Search
-          </AntButton>
-          <AntButton
-            onClick={() => clearFilters && handleReset(clearFilters)}
-            size="small"
-            style={{
-              width: 90,
-            }}
-          >
-            Reset
-          </AntButton>
-          <AntButton
-            type="link"
-            size="small"
-            onClick={() => {
-              confirm({
-                closeDropdown: false,
-              });
-              setSearchText(selectedKeys[0]);
-              setSearchedColumn(dataIndex);
-            }}
-          >
-            Filter
-          </AntButton>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered) => (
-      <SearchOutlined
-        style={{
-          color: filtered ? "#1890ff" : undefined,
-        }}
-      />
-    ),
-    onFilter: (value, record) =>
-      record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
-    onFilterDropdownVisibleChange: (visible) => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.select(), 100);
-      }
-    },
-    render: (text) =>
-      searchedColumn === dataIndex ? (
-        <Highlighter
-          highlightStyle={{
-            backgroundColor: "#ffc069",
-            padding: 0,
-          }}
-          searchWords={[searchText]}
-          autoEscape
-          textToHighlight={text ? text.toString() : ""}
-        />
-      ) : (
-        text
-      ),
-  });
 
   const columns = [
     {
-      title: "Name",
-      dataIndex: "name",
+      headerName: "Name",
+      field: "name",
       key: "name",
       // width: "30%",
-      ...getColumnSearchProps("name"),
+      // ...getColumnSearchProps("name"),
     },
     {
-      title: "Link",
-      dataIndex: "age",
-      key: "age",
+      headerName: "Link",
+      field: "link",
+      key: "link",
+      renderCell: (row) => (
+        <a target="_blank" rel="noreferrer" href={row.value}>
+          {row.value}
+        </a>
+      ),
       // width: "20%",
-      ...getColumnSearchProps("age"),
+      // ...getColumnSearchProps("age"),
     },
     {
-      title: "Followers",
-      dataIndex: "followers",
+      headerName: "Followers",
+      field: "followers",
       key: "age",
       // width: "20%",
       // ...getColumnSearchProps("age"),
-      sorter: (a, b) => a - b,
-      sortDirections: ["descend", "ascend"],
+      // sorter: (a, b) => a - b,
+      // sortDirections: ["descend", "ascend"],
     },
     {
-      title: "Avg. Views",
-      dataIndex: "avgView",
-      key: "avgView",
-      sorter: (a, b) => a - b,
-      sortDirections: ["descend", "ascend"],
+      headerName: "Avg. Views",
+      field: "avgViews",
+      key: "avgViews",
+      // sorter: (a, b) => a - b,
+      // sortDirections: ["descend", "ascend"],
     },
     {
-      title: "Deliverable",
-      dataIndex: "deliverable",
+      headerName: "Deliverable",
+      field: "deliverable",
       key: "deliverable",
       // width: "20%",
-      ...getColumnSearchProps("deliverable"),
+      // ...getColumnSearchProps("deliverable"),
     },
     {
-      title: "Commercial Creator",
-      dataIndex: "commerciaCreator",
-      key: "commerciaCreator",
+      headerName: "Commercial Creator",
+      field: "commercialCreator",
+      key: "commercialCreator",
       // width: "20%",
-      ...getColumnSearchProps("commerciaCreator"),
+      // ...getColumnSearchProps("commerciaCreator"),
     },
   ];
 
   return (
-    <MainLayout classes={[styles.container]}>
-      <Navbar
-        titleProps={{
+    <MainLayout
+      classes={[styles.container]}
+      navbarProps={{
+        titleProps: {
           title: "Campaigns",
-          // isEditIconVisible: true,
-          // isBackIconVisible: true,
-          // brandName: "Nike",
-        }}
-        //   progress={progress}
-      />
+        },
+      }}
+    >
       <div className={styles.tableContainer}>
-        <Table columns={columns} dataSource={data} />
+        <CustomTable columns={columns} data={data} />
       </div>
     </MainLayout>
   );
