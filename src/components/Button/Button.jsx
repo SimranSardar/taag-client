05586363dd8fbfa9ebe8c36d398a0a723fca549a
Anurag Pal <apal895@gmail.com>
2022-07-16 22:57:47 +0530

    added onClick

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,12 @@
 import styles from "./Button.module.scss";
 
-const Button = ({ children, leftIcon, rightIcon, type }) => {
+const Button = ({ children, leftIcon, rightIcon, type, onClick }) => {
   return (
-    <button type={type ? type : "button"} className={styles.btn}>
+    <button
+      type={type ? type : "button"}
+      className={styles.btn}
+      onClick={onClick}
+    >
       <span>
         {leftIcon}
         <span>{children}</span>
